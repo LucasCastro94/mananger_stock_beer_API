9e54bb872cf44d3a4bd0071138c08e0f7a87ddc0
LucasCastro <lucascastro_94@hotmail.com>
2023-02-21 10:08:51 -0300

    starter

---
+++ src/main/java/lucascastro/git/com/Mananger_Beer_API/ManangerBeerApiApplication.java
@@ -0,0 +1,13 @@
+package lucascastro.git.com.Mananger_Beer_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManangerBeerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManangerBeerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lucascastro/git/com/Mananger_Beer_API/ManangerBeerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lucascastro.git.com.Mananger_Beer_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManangerBeerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
